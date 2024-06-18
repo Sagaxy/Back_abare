5b2b20834071692fd31a3af8378bb7e26159708a
joao vitor melo <jvvitor307@gmail.com>
2024-06-18 19:06:56 -0300

    adicionando classes para o modelo

--- schemas/user.py
+++ schemas/user.py
@@ -35,17 +35,57 @@ class Child(Base):
     gender = Column(String)
     spectrum_degree = Column(String)
     #family_id = Column(Integer)
-    trait = Column(String)
+    #foreign
+    trait_id = Column(String)
 
 class Tag(Base):
-    __tabblename__ = "tags"
+    __tablename__ = "tags"
     id = Column(Integer, prymary_key=True)
     value = Column(String)
     #foreign
     tag_kind_id = Column(Integer, )
     
 class TagKind(Base):
-    __tabblename__ = "tag-kind"
+    __tablename__ = "tag-kind"
     id = Column(Integer, prymary_key = True)
     kind = Column(String)
-        
+
+class Traits(Base):
+    __tablename__ = "traits"
+    id = Column(Integer, prymary_key=True)
+    value = Column(String)
+
+class TraitsTags(Base):
+    __tablename__ = "traits-tags"
+    #foreign
+    trait_id = Column(Integer)
+    #foreign
+    tag_id = Column(Integer)
+
+class SchoolAdministrators(Base):
+    __tablename__ = "school-administrators"
+    id = Column(Integer, prymary_key = True)
+    #foreign
+    admin_user_id  = Column(Integer)
+    #foreign
+    school_id = Column(Integer)
+
+class Schools(Base):
+    __tablename__ = "schools"
+    id = Column(Integer, prymary_key = True )
+    name = Column(String)
+    address = Column(String)
+    contact_email = Column(String)
+    contact_telephone = Column(String)
+
+#isso vem de usuario ou nao tem relacionamento
+class schoolProfessionals(Base):
+    __tablename__ = "schools-professionals"
+    id = Column(Integer, prymary_key = True )
+    name = Column(String)
+    email = Column(String)
+    role = Column(String)
+    cpf = Column(String)
+    address = Column(String)
+    #fk
+    school_id = Column(Integer)
